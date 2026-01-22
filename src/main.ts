f68eddc190708c99e1c3d86bb73ed7806bd89bfd
pulidoepee <diegojesuspulido@gmail.com>
2026-01-22 11:05:05 +0000

    Proyecto final - Backend

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo List API')
+    .setDescription('Todo list aplication')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
+      'jwt',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
